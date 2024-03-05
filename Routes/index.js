4e90368b22f5f5090ea6cdb26321c3653a9b3037
Robert James Newell <112651212+robjam1990@users.noreply.github.com>
2024-03-05 14:07:49 -0500

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -1,24 +1,45 @@
-// File = robjam1990/Psychosis/Routes/index.js
+// routes/index.js
 const express = require('express');
-const router = express.Router();
-const { validationResult } = require('express-validator');
+const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
 
-// Define index route
+const router = express.Router();
+
+// Middleware
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+// Global validation middleware
+router.use([
+    body('username').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
+]);
+
+router.use((req, res, next) => {
+    console.log('Custom middleware');
+    next();
+});
+
+// Routes
+router.use('/', require('./about'));
+router.use('/contact', require('./contact'));
+router.use('/products', require('./products'));
+
 router.get('/', (req, res) => {
     res.send('Welcome to the main index');
 });
 
-// Example of input validation middleware
-router.post('/example', [
-    body('username').isEmail(),
-    body('password').isLength({ min: 5 }),
-], (req, res) => {
+router.post('/example', (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    // Process valid input
     res.send('Valid input');
 });
+// errorHandling.js
+module.exports = function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+};
 
 module.exports = router;
